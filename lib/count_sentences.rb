cf2dad3664d0a06389f8731726d5ec8058fcdf49
Katherine Walendzik <katherinewalendzik@gmail.com>
2021-04-09 19:39:02 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -5,16 +5,16 @@ class String
   def sentence?
     self.end_with?(".")
   end
-
+  
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.count_sentences
   end
 end
